8381b8c7674632ed08769b2c1d2bff2bf549eab5
Frankson <frankson_123@hotmail.com>
2020-07-02 02:21:28 -0300

    Implementado função que cria posições randomicas para o mosquito respeitando as dimensões da tela

--- jogo.js
+++ jogo.js
@@ -4,6 +4,7 @@ var largura = 0
 
 
 function ajustaTamanhoPalcoJogo(){
+    //capturando altura e largura da tela
      altura = window.innerHeight
      largura = window.innerWidth
      console.log(altura, largura)
@@ -12,3 +13,24 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica(){
+
+var posicaoX = Math.floor(Math.random() * largura) -90
+var posicaoY = Math.floor(Math.random() * altura) -90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+console.log(posicaoX, posicaoY)
+
+//Criar o elemento html:
+
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosquito.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
+}
